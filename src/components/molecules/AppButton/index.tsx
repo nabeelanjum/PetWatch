4a253e4c96f989fd1aa206fb8410f4b7610db9d0
Nabeel Anjum <timedilation2012@gmail.com>
2025-07-12 20:45:14 +0500

    make (atoms): app button component

---
+++ src/components/molecules/AppButton/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ActivityIndicator,
+} from 'react-native';
+import { AppText } from '../../atoms';
+import styles from './styles';
+
+export type AppButtonProps = TouchableOpacityProps & {
+  title?: string;
+  isLoading?: boolean;
+};
+
+const AppButton: React.FC<AppButtonProps> = ({
+  title,
+  style,
+  isLoading,
+  disabled,
+  ...props
+}) => {
+  return (
+    <TouchableOpacity
+      {...props}
+      style={StyleSheet.flatten([styles.container, style])}
+      disabled={disabled || isLoading}
+    >
+      {isLoading ? <ActivityIndicator /> : <AppText>{title}</AppText>}
+    </TouchableOpacity>
+  );
+};
+
+export default AppButton;
---
+++ src/components/molecules/AppButton/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../../core/constants/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 100,
+    backgroundColor: colors.brand.orange,
+    width: '100%',
+    height: 50,
+  },
+});
+
+export default styles;
